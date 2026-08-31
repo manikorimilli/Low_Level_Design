dae65453d0629e2e04c5a2002272e7c2af68634b
manikorimilli <striverzarc@gmail.com>
2026-08-31 15:32:39 +0530

    updated: learned the OCP

--- src/_02Solid_Principles/_02Open_Closed_Principle.java
+++ src/_02Solid_Principles/_02Open_Closed_Principle.java
@@ -1,4 +1,34 @@
 package _02Solid_Principles;
 
+
+interface TaxCalculator{
+    double calculateAfterTax(double amount);
+}
+
+class IndianTax implements TaxCalculator{
+
+    public double calculateAfterTax(double amount){
+        return (amount + 0.18*amount);
+    }
+}
+
+class UsTax implements TaxCalculator{
+    public double calculateAfterTax(double amount){
+        return (amount + 0.10*amount);
+    }
+}
 public class _02Open_Closed_Principle {
+    public static void main(String[] args) {
+
+        //Indian Tax
+        TaxCalculator india=new IndianTax();
+        System.out.println(india.calculateAfterTax(1000));
+
+        //Us Tax
+        TaxCalculator us=new UsTax();
+        System.out.println(us.calculateAfterTax(1000));
+
+
+
+    }
 }
